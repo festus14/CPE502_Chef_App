08143df4854e0fd5ff2ae187443105ed4d6193ba
Oguamanam Ebube <ed.oguamanam@gmail.com>
2021-09-10 21:28:06 +0100

    Added User Seed

--- app/Database/Seeds/Customer.php
+++ app/Database/Seeds/Customer.php
@@ -3,11 +3,22 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use App\Models\User as ModelsUser;
 
 class Customer extends Seeder
 {
 	public function run()
 	{
-		//
+		$userModel = new ModelsUser();
+		$user = $userModel->asArray()->where('role', 'customer')->first();
+
+		$data = [
+			'user_id' => $user['id'],
+			'user_name' => 'Moneyman',
+			'address' => '90, Beverly Hills, Banana Island',
+		];
+
+		// Using Query Builder
+		$this->db->table('customers')->insert($data);
 	}
 }
